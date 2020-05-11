494032527b793d34a8facbf0b446803b97f87ac5
Minder <51049588+490700113@users.noreply.github.com>
2020-05-12 00:39:40 +0800

    text

--- tanks war/Map.h
+++ tanks war/Map.h
@@ -14,10 +14,10 @@ class Map
 {
 private:
 	char map[map_row_px][map_col_px];//地图数组
-//	int Level;//关卡数    --后期设置一个关卡选择界面
+	void chooseMap(int level = 1);//选择关卡，level是关卡数
 public:
 	Map();//构造函数
 
-	//关卡选择
-	void chooseMap(int level = 1);//选择关卡，level是关卡数
+	void ChangeLevel(int level);//切换关卡的函数
+
 };
